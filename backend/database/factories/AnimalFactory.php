bf708882565d5ed67b6e188c3664bb08ac349acf
João Angelo <joaoangelo409@gmail.com>
2023-10-25 19:53:31 -0300

    feature: factory e seeder de animal

--- backend/database/factories/AnimalFactory.php
+++ backend/database/factories/AnimalFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Owner;
+use App\Models\Race;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,10 @@ class AnimalFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            "name" => $this->faker->word(),
+            "birth_date" => $this->faker->date(),
+            "owner_id" => Owner::factory(),
+            "race_id" => Race::factory()
         ];
     }
 }
