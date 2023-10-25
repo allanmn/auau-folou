c3a81310b1edc06e4989c8f95dadc832e52d2597
João Angelo <joaoangelo409@gmail.com>
2023-10-25 19:52:15 -0300

    fix: down com migrations e typos de plural de tabela

--- backend/database/migrations/2023_10_23_220743_create_race_table.php
+++ backend/database/migrations/2023_10_23_220743_create_race_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('race');
+        Schema::dropIfExists('animals');
+        Schema::dropIfExists('races');
     }
 };
